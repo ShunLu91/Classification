0da0489fb161325a22c2dcbae2961bfb38ca5838
lushun_mac <470651748@qq.com>
2020-07-06 00:17:51 +0800

    classify06 shuffle

--- classify06/kuzu_main.py
+++ classify06/kuzu_main.py
@@ -94,7 +94,10 @@ def main():
     kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
 
     # Define a transform to normalize the data
-    transform = transforms.Compose([transforms.ToTensor(),
+    transform = transforms.Compose([
+                                    transforms.RandomCrop(32, padding=4),
+                                    transforms.RandomHorizontalFlip(),
+                                    transforms.ToTensor(),
                                     transforms.Normalize((0.5,), (0.5,))])
 
     # Fetch and load the training data
