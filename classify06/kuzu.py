1b99501cb18b439121bf268626f3bb9bf778e9fa
lushun_mac <470651748@qq.com>
2020-07-06 00:25:43 +0800

    classify06 shuffle

--- classify06/kuzu.py
+++ classify06/kuzu.py
@@ -43,13 +43,13 @@ class NetConv(nn.Module):
         channel = [16, 32]
         self.conv1 = nn.Sequential(nn.Conv2d(1, channel[0], 3, 2, 1), nn.BatchNorm2d(channel[0]), nn.ReLU())
         self.conv2 = nn.Sequential(nn.Conv2d(channel[0], channel[1], 3, 2, 1), nn.BatchNorm2d(channel[1]), nn.ReLU())
-        # self.dropout = nn.Dropout()
+        self.dropout = nn.Dropout()
         self.fc = nn.Sequential(nn.Linear(channel[1] * 7 * 7, 10), nn.ReLU())
 
     def forward(self, x):
         x = self.conv1(x)
         x = self.conv2(x)
         x = x.view(x.size()[0], -1)
-        # x = self.dropout(x)
+        x = self.dropout(x)
         x = self.fc(x)
         return F.log_softmax(x)
