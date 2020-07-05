e4ed86a718ca0d1175890821da521602022d3d52
lushun_mac <470651748@qq.com>
2020-07-06 00:18:57 +0800

    classify06 shuffle

--- classify06/kuzu_main.py
+++ classify06/kuzu_main.py
@@ -95,7 +95,7 @@ def main():
 
     # Define a transform to normalize the data
     transform = transforms.Compose([
-                                    transforms.RandomCrop(32, padding=4),
+                                    transforms.RandomCrop(28, padding=4),
                                     transforms.RandomHorizontalFlip(),
                                     transforms.ToTensor(),
                                     transforms.Normalize((0.5,), (0.5,))])
