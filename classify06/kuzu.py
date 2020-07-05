3a1e6c390834f655ed88da65175be78deecc010a
lushun_mac <470651748@qq.com>
2020-07-05 23:43:31 +0800

    classify06

--- classify06/kuzu.py
+++ classify06/kuzu.py
@@ -42,13 +42,13 @@ class NetConv(nn.Module):
         super(NetConv, self).__init__()
         self.conv1 = nn.Sequential(nn.Conv2d(1, 32, 3, 2, 1), nn.BatchNorm2d(32), nn.ReLU())
         self.conv2 = nn.Sequential(nn.Conv2d(32, 64, 3, 2, 1), nn.BatchNorm2d(64), nn.ReLU())
-        self.dropout = nn.Dropout(0.7)
-        self.fc = nn.Sequential(nn.Linear(64 * 7 * 7, 10))
+        # self.dropout = nn.Dropout()
+        self.fc = nn.Sequential(nn.Linear(64 * 7 * 7, 10), nn.ReLU())
 
     def forward(self, x):
         x = self.conv1(x)
         x = self.conv2(x)
         x = x.view(x.size()[0], -1)
-        x = self.dropout(x)
+        # x = self.dropout(x)
         x = self.fc(x)
         return F.log_softmax(x)
