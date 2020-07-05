ed7eb619a891b5fcff5dc316e7fa66df70fd0e9e
lushun_mac <470651748@qq.com>
2020-07-06 00:38:13 +0800

    classify06 shuffle

--- classify06/kuzu_main.py
+++ classify06/kuzu_main.py
@@ -115,7 +115,7 @@ def main():
     if list(net.parameters()):
         optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=args.mom, weight_decay=1e-3)
         scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
-            optimizer, float(args.epochs), eta_min=args.learning_rate_min, last_epoch=-1)
+            optimizer, float(args.epochs), eta_min=0.0001, last_epoch=-1)
         for epoch in range(1, args.epochs + 1):
             train(args, net, device, train_loader, optimizer, epoch)
             test(args, net, device, test_loader)
