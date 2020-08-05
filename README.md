5d788a6756886e3758aa8f1e15b579311dde9aae
lushun_mac <470651748@qq.com>
2020-08-05 11:17:35 +0800

    move file

--- README.md
+++
@@ -1,2 +0,0 @@
-# Classification
-Classification_Pytorch
--- run.sh
+++
@@ -1,2 +0,0 @@
-nohup python -u transfer.py --exp_name transfer1 --model_name resnet50 --gpu 3  --dropout_rate 0.5 --cutout \
- --epochs 100 --weight_decay 3e-4 --learning_rate 0.001 --batch_size 128  > log1  2>&1 &
