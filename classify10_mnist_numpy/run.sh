331c5a1a4663facaa1f8663f32549ff1024bc2c0
lushun_mac <470651748@qq.com>
2020-10-14 00:51:11 +0800

    layer4

--- classify10_mnist_numpy/run.sh
+++ classify10_mnist_numpy/run.sh
@@ -1,2 +1,3 @@
 #nohup python -u classify10_mnist_numpy/mlp_mnist.py > ./logdir/baseline.log  2>&1 &
-nohup python -u classify10_mnist_numpy/mlp_mnist.py > ./logdir/layer2.log  2>&1 &
+#nohup python -u classify10_mnist_numpy/mlp_mnist.py > ./logdir/layer2.log  2>&1 &
+nohup python -u classify10_mnist_numpy/mlp_mnist.py > ./logdir/layer4.log  2>&1 &
