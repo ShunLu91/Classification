b43c3b626578c2f59384c6b5802238764b36677f
lushun_mac <470651748@qq.com>
2020-10-15 00:50:36 +0800

    layer4/5/6

--- classify10_mnist_numpy/work2/run.sh
+++ classify10_mnist_numpy/work2/run.sh
@@ -1,4 +1,4 @@
-nohup python -u main.py --dimension 128 64 10 > ./logdir/baseline.log  2>&1 &
-nohup python -u main.py --dimension 128 64 64 10 > ./logdir/layer4.log  2>&1 &
-nohup python -u main.py --dimension 128 64 64 64 10 > ./logdir/layer5.log  2>&1 &
-nohup python -u main.py --dimension 128 64 64 64 64 10 > ./logdir/layer6.log  2>&1 &
+nohup python -u main.py --layers 3 --dimension 128 64 10 > ./logdir/baseline.log  2>&1 &
+nohup python -u main.py --layers 4 --dimension 128 64 64 10 > ./logdir/layer4.log  2>&1 &
+nohup python -u main.py --layers 5 --dimension 128 64 64 64 10 > ./logdir/layer5.log  2>&1 &
+nohup python -u main.py --layers 6 --dimension 128 64 64 64 64 10 > ./logdir/layer6.log  2>&1 &
