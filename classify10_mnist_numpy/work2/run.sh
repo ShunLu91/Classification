1d13835494d0f3d14a7833d666a1bf55e3679fb4
lushun_imac <470651748@qq.com>
2020-10-15 13:42:42 +0800

    neuron0/1/2

--- classify10_mnist_numpy/work2/run.sh
+++ classify10_mnist_numpy/work2/run.sh
@@ -1,4 +1,12 @@
-nohup python -u main.py --layers 3 --dimension 128 64 10 > ./logdir/baseline.log  2>&1 &
-nohup python -u main.py --layers 4 --dimension 128 64 64 10 > ./logdir/layer4.log  2>&1 &
-nohup python -u main.py --layers 5 --dimension 128 64 64 64 10 > ./logdir/layer5.log  2>&1 &
-nohup python -u main.py --layers 6 --dimension 128 64 64 64 64 10 > ./logdir/layer6.log  2>&1 &
+## baseline
+#nohup python -u main.py --layers 3 --dimension 128 64 10 > ./logdir/baseline.log  2>&1 &
+#
+## layers
+#nohup python -u main.py --layers 4 --dimension 128 64 64 10 > ./logdir/layer4.log  2>&1 &
+#nohup python -u main.py --layers 5 --dimension 128 64 64 64 10 > ./logdir/layer5.log  2>&1 &
+#nohup python -u main.py --layers 6 --dimension 128 64 64 64 64 10 > ./logdir/layer6.log  2>&1 &
+
+# neurons
+nohup python -u main.py --layers 3 --dimension 64 32 10 > ./logdir/neuron0.log  2>&1 &
+nohup python -u main.py --layers 3 --dimension 256 128 10 > ./logdir/neuron1.log  2>&1 &
+nohup python -u main.py --layers 3 --dimension 512 256 10 > ./logdir/neuron2.log  2>&1 &
