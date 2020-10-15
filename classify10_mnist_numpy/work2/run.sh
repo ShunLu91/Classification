97c40181e28c943d43a980b59a0f2d477ce4d191
lushun_imac <470651748@qq.com>
2020-10-15 14:09:19 +0800

    wd0/1/2, dp0/1/2

--- classify10_mnist_numpy/work2/run.sh
+++ classify10_mnist_numpy/work2/run.sh
@@ -7,6 +7,16 @@
 #nohup python -u main.py --layers 6 --dimension 128 64 64 64 64 10 > ./logdir/layer6.log  2>&1 &
 
 # neurons
-nohup python -u main.py --layers 3 --dimension 64 32 10 > ./logdir/neuron0.log  2>&1 &
-nohup python -u main.py --layers 3 --dimension 256 128 10 > ./logdir/neuron1.log  2>&1 &
-nohup python -u main.py --layers 3 --dimension 512 256 10 > ./logdir/neuron2.log  2>&1 &
+#nohup python -u main.py --layers 3 --dimension 64 32 10 > ./logdir/neuron0.log  2>&1 &
+#nohup python -u main.py --layers 3 --dimension 256 128 10 > ./logdir/neuron1.log  2>&1 &
+#nohup python -u main.py --layers 3 --dimension 512 256 10 > ./logdir/neuron2.log  2>&1 &
+
+# weight_decay
+nohup python -u main.py --layers 3 --dimension 128 64 10 --weight_decay 3e-2 > ./logdir/wd0.log  2>&1 &
+nohup python -u main.py --layers 3 --dimension 128 64 10 --weight_decay 3e-3 > ./logdir/wd1.log  2>&1 &
+nohup python -u main.py --layers 3 --dimension 128 64 10 --weight_decay 3e-5 > ./logdir/wd2.log  2>&1 &
+
+# dropout
+nohup python -u main.py --layers 3 --dimension 128 64 10 --dropout_rate 0.3 > ./logdir/dp0.log  2>&1 &
+nohup python -u main.py --layers 3 --dimension 128 64 10 --dropout_rate 0.6 > ./logdir/dp1.log  2>&1 &
+nohup python -u main.py --layers 3 --dimension 128 64 10 --dropout_rate 0.9 > ./logdir/dp2.log  2>&1 &
